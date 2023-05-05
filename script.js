c14bada2dfed7106712d63b4465ce8beb7708e79
haileyrs <haileyscheevel@gmail.com>
2023-05-05 12:35:58 -0500

    complete gameplay functionality and winner banner

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const GameBoard = (p1, p2) => {
   let turns = 0;
-  let currentPlayer;
+  let currentPlayer = p1;
+  let gameOver = false;
 
   let grid = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
   ];
 
   const Square = (row, col) => {
@@ -18,16 +19,17 @@ const GameBoard = (p1, p2) => {
   };
 
   const setUp = () => {
-    currentPlayer = p1;
+    updatePlayer(p1);
     const board = document.querySelector(".game-grid");
     board.innerHTML = "";
     let col = 0;
     let row = 0;
     grid = [
-      [0, 0, 0],
-      [0, 0, 0],
-      [0, 0, 0],
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
     ];
+    gameOver = false;
     for (i = 0; i < 9; i++) {
       const square = Square(row, col);
       square.element.classList.add("grid-square");
@@ -43,21 +45,23 @@ const GameBoard = (p1, p2) => {
 
   const resetGame = () => {
     turns = 0;
+    banner.innerHTML = '';
+    banner.style.visibility = 'hidden';
     setUp();
   };
 
   const updatePlayer = (player) => {
     currentPlayer = player;
-    // change dialog on bottom
-    const infoText = document.querySelector('.info-text')
+    const infoText = document.querySelector(".info-text");
     infoText.className = player.shape;
-    infoText.classList.add('info-text');
-    infoText.innerHTML = 'Player ' + player.id + ': ' + player.shape
+    infoText.classList.add("info-text");
+    infoText.innerHTML =
+      "Player " + player.id + ": " + player.shape.toUpperCase();
   };
 
   const fillSquare = (sqr) => {
     // fill square logic
-    if (sqr.filled === false) {
+    if (!sqr.filled && !gameOver) {
       const mark = document.createElement("p");
       mark.textContent = currentPlayer.shape;
       mark.classList.add(currentPlayer.shape);
@@ -68,28 +72,37 @@ const GameBoard = (p1, p2) => {
       grid[sqr.row][sqr.col] = currentPlayer.shape;
       console.log(grid);
       turns += 1;
-      updatePlayer(currentPlayer === p1 ? p2 : p1);
-    }
-
-    if (turns > 4) {
-      checkForWin();
+      if (turns > 4) {
+        checkForWin();
+      }
+      if (!gameOver) {
+        updatePlayer(currentPlayer === p1 ? p2 : p1);
+      }
     }
   };
 
   const checkForWin = () => {
     if (
-      (grid[0][0] == grid[0][1]) == grid[0][2] ||
-      (grid[1][0] == grid[1][1]) == grid[1][2] ||
-      (grid[2][0] == grid[2][1]) == grid[2][2] ||
-      (grid[0][0] == grid[1][0]) == grid[2][0] ||
-      (grid[0][1] == grid[1][1]) == grid[2][1] ||
-      (grid[0][2] == grid[1][2]) == grid[2][2] ||
-      (grid[0][0] == grid[1][1]) == grid[2][2] ||
-      (grid[0][2] == grid[1][1]) == grid[2][0]
+      (grid[0][0] == grid[0][1] && grid[0][1] == grid[0][2]) ||
+      (grid[1][0] == grid[1][1] && grid[1][1] == grid[1][2]) ||
+      (grid[2][0] == grid[2][1] && grid[2][1] == grid[2][2]) ||
+      (grid[0][0] == grid[1][0] && grid[1][0] == grid[2][0]) ||
+      (grid[0][1] == grid[1][1] && grid[1][1] == grid[2][1]) ||
+      (grid[0][2] == grid[1][2] && grid[1][2] == grid[2][2]) ||
+      (grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2]) ||
+      (grid[0][2] == grid[1][1] && grid[1][1] == grid[2][0])
     ) {
-      console.log("game over");
+      gameOver = true;
+      const text = document.createElement('h2')
+      text.textContent = 'Winner: Player ' + currentPlayer.id
+      banner.appendChild(text);
+      banner.style.visibility = 'visible';
     } else if (turns == 9) {
-      console.log("it's a tie");
+      gameOver = true;
+      const text = document.createElement('h2')
+      text.textContent = "It's a tie!"
+      banner.appendChild(text);
+      banner.style.visibility = 'visible';
     }
   };
 
@@ -101,9 +114,12 @@ const Player = (id, shape) => {
 };
 
 const resetBtn = document.querySelector(".reset");
+const banner = document.querySelector('.banner');
 
 resetBtn.addEventListener("click", () => board.resetGame());
 
 const player1 = Player(1, "x");
 const player2 = Player(2, "o");
 const board = GameBoard(player1, player2);
+
+board.resetGame()
